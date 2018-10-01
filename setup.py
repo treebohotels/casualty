d54b390808094652633d44c5cbbe39044ea82b56
keshrisohit <sumitk002@gmail.com>
2018-10-01 13:16:16 +0530

    added pre commit hook

--- setup.py
+++ setup.py
@@ -1,17 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='corelated_logs',
-    version='0.0.5',
-    packages=find_packages(exclude=['tests']),
-    install_requires=[
-        'structlog==18.2.0',
-        'wrapt==1.10.11'
-
-    ],
-    url='',
-    license='BSD',
-    author='sohit kumar',
-    author_email='sumitk002@gmail.com',
+    name="corelated_logs",
+    version="0.0.5",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=["structlog==18.2.0", "wrapt==1.10.11", "pre-commit-hooks==1.4.0"],
+    url="",
+    license="BSD",
+    author="sohit kumar",
+    author_email="sumitk002@gmail.com",
     test_suite="tests",
-    description='A python library to generate corelation id and bind it to headers in outgoing request')
+    description="A python library to generate corelation id and bind it to headers in outgoing request",
+)
