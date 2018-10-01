beabce155200da5cbbaf89e70ebdb43ce3890b84
keshrisohit <sumitk002@gmail.com>
2018-10-01 13:35:33 +0530

    fixed compile time error

--- corelated_logs/django_corelation_middleware.py
+++ corelated_logs/django_corelation_middleware.py
@@ -1,19 +1,20 @@
 import structlog
+import uuid
 
 
 class DjangoCorelationMiddleware(object):
-    CORELATED_HEADER = 'HTTP_X_CO_REQUEST_ID'
+    CORELATED_HEADER = "HTTP_X_CO_REQUEST_ID"
 
     def __init__(self, app):
         self.app = app
         structlog.configure(
             processors=[
-                structlog.processors.TimeStamper(fmt='ISO'),
-                structlog.processors.JSONRenderer()
+                structlog.processors.TimeStamper(fmt="ISO"),
+                structlog.processors.JSONRenderer(),
             ],
             context_class=structlog.threadlocal.wrap_dict(dict),
             wrapper_class=structlog.stdlib.BoundLogger,
-            cache_logger_on_first_use=True
+            cache_logger_on_first_use=True,
         )
 
     def process_request(self, request):
