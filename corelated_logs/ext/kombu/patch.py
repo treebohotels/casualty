7e8810ca2d06e685fa206b81044de02feaf23c75
keshrisohit <sumitk002@gmail.com>
2018-10-02 13:45:42 +0530

    struct log initalizer for kombu

--- corelated_logs/ext/kombu/patch.py
+++ corelated_logs/ext/kombu/patch.py
@@ -3,6 +3,7 @@ import logging
 import structlog
 import wrapt
 import uuid
+import json
 
 from corelated_logs import constants
 
@@ -11,9 +12,21 @@ logger = logging.getLogger()
 
 def patch():
     wrapt.wrap_function_wrapper("kombu", "messaging.Producer.publish", _inject_header)
+    wrapt.wrap_function_wrapper(
+        "kombu", "messaging.Consumer.__init__", _initialize_structlog_configuration
+    )
+    wrapt.wrap_function_wrapper(
+        "kombu",
+        "messaging.Consumer._receive_callback",
+        _bind_request_id_on_message_recieve,
+    )
 
 
 def _inject_header(wrapped, instance, args, kwargs):
+
+    if "type" in args[0] and args[0]["type"] == "worker-heartbeat":
+        return wrapped(*args, **kwargs)
+
     if "headers" in kwargs:
         headers = kwargs["headers"]
     else:
@@ -26,4 +39,36 @@ def _inject_header(wrapped, instance, args, kwargs):
     except Exception as e:
         headers[constants.REQUEST_HEADER] = str(uuid.uuid4())
     kwargs["headers"] = headers
+    return wrapped(*args, **kwargs)
+
+
+def _initialize_structlog_configuration(wrapped, instance, args, kwargs):
+    structlog.configure(
+        processors=[
+            structlog.processors.TimeStamper(fmt="ISO"),
+            structlog.processors.JSONRenderer(),
+        ],
+        context_class=structlog.threadlocal.wrap_dict(dict),
+        wrapper_class=structlog.stdlib.BoundLogger,
+        cache_logger_on_first_use=True,
+    )
+
+    return wrapped(*args, **kwargs)
+
+
+def _bind_request_id_on_message_recieve(wrapped, instance, args, kwargs):
+
+    try:
+        if json.loads(args[0].body)["type"] == "worker-heartbeat":
+            return wrapped(*args, **kwargs)
+    except Exception as e:
+        pass
+
+    logger = structlog.getLogger()
+    try:
+        request_id = args[1].headers[constants.REQUEST_HEADER]
+    except Exception as e:
+        request_id = str(uuid.uuid4())
+
+    logger = logger.bind(request_id=request_id)
     return wrapped(*args, **kwargs)
